73bc5a8e6bc8e2f988b0296c7c0d705ce0bb61bb
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-10-30 16:32:15 -0700

    view email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -19,11 +19,44 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#details-view').style.display = 'none';
 
-  // Clear out composition fields
+  // Clear out composition fields 
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+}
+
+function view_email(id) {
+  fetch(`/emails/${id}`)
+.then(response => response.json())
+.then(email => {
+    // Print email
+    console.log(email);
+    
+    document.querySelector(`#emails-view`).style.display = 'none';
+    document.querySelector(`#compose-view`).style.display = 'none';
+    document.querySelector(`#details-view`).style.display = 'block';
+
+    document.querySelector(`#details-view`).innerHTML = `
+      <strong>From: </strong>${email.sender}<br>
+      <strong>To: </strong>${email.recipients}<br>
+      <strong>Subject: </strong>${email.subject}<br>
+      <strong>Timestamp: </strong>${email.timestamp}<br>
+    <br><hr>
+      ${email.body}
+      `
+
+    // Change to read
+    if(!email.read){
+      fetch(`/emails/${email.id}`, {
+        method: 'PUT',
+        body:JSON.stringify({
+          read:true
+        })
+      })
+    }
+  });
 }
 
 function load_mailbox(mailbox) {
@@ -31,6 +64,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#details-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -42,7 +76,7 @@ fetch(`/emails/${mailbox}`)
   //loop through emails and create a div for each 
   emails.forEach(singleEmail => {
 
-    //create div for each email
+    //Create div for each email
     const newEmail = document.createElement('div');
     newEmail.className = "list-group-item";
     newEmail.innerHTML = `
@@ -51,12 +85,12 @@ fetch(`/emails/${mailbox}`)
       <p>${singleEmail.timestamp}</p>
     `;
 
-    //toggle read or not
+    //Toggle read or not
     newEmail.className = singleEmail.read ? 'read' : 'undread';
     //add click event to view email
     newEmail.addEventListener('click', function() {
-    console.log('This element has been clicked!')
-});
+      view_email(singleEmail.id)
+    }); 
 document.querySelector('#emails-view').append(newEmail);
   })
 });
@@ -93,4 +127,3 @@ function send_email(event){
 
 }
 
-function view_email()
